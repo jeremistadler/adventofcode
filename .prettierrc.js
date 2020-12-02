4fb18b736e39842b302421e79ca132db8f988dd5
Jeremi Stadler <jeremi.stadler@storytel.com>
2020-12-02 06:41:42 +0100

    Add initial config stuff

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+// Duplicated from react repo: https://github.com/facebook/react/blob/master/.prettierrc.js
+module.exports = {
+  bracketSpacing: false,
+  singleQuote: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid',
+  trailingComma: 'all',
+  semi: false,
+  printWidth: 80,
+}

62c45bf0f38a4d4add3343ce5155e0f7082802e4
Jeremi Stadler <jeremi.stadler@storytel.com>
2020-12-12 07:09:44 +0100

    2020 Day 11, Add part 1

--- 11/index.js
+++ 11/index.js
@@ -1,324 +1,299 @@
-let fs = require('fs')
-
-let current = fs
+let lines = require('fs')
   .readFileSync('./11/input', {encoding: 'utf-8'})
   .split('\n')
-  .filter(f => f.trim().length > 0)
+  .map(f => f.trim())
+  .filter(f => f.length > 0)
   .map(f => f.split(''))
 
-let next = current.map(f => f.slice())
+const WIDTH = lines[0].length
+const HEIGHT = lines.length
 
-const WIDTH = current[0].length
-const HEIGHT = current.length
+function part1() {
+  let current = lines.map(f => f.slice())
+  let next = current.map(f => f.slice())
 
-function hasAdjacentOccupied(grid, x, y) {
-  for (let xx = x + 1; xx <= WIDTH - 1; xx++) {
-    if (grid[y][xx] === '#') return true
-    if (grid[y][xx] === 'L') break
-  }
-
-  for (let yy = y + 1; yy <= HEIGHT - 1; yy++) {
-    if (grid[yy][x] === '#') return true
-    if (grid[yy][x] === 'L') break
-  }
-
-  for (let xx = x - 1; xx >= 0; xx--) {
-    if (grid[y][xx] === '#') return true
-    if (grid[y][xx] === 'L') break
-  }
-
-  for (let yy = y - 1; yy >= 0; yy--) {
-    if (grid[yy][x] === '#') return true
-    if (grid[yy][x] === 'L') break
-  }
-
-  //
-
-  for (let d = 1; ; d++) {
-    let xx = x + d
-    let yy = y + d
-    if (xx > WIDTH - 1) break
-    if (yy > HEIGHT - 1) break
-
-    if (grid[yy][xx] === '#') return true
-    if (grid[yy][xx] === 'L') break
-  }
-
-  for (let d = 1; ; d++) {
-    let xx = x + d
-    let yy = y - d
-    if (xx > WIDTH - 1) break
-    if (yy < 0) break
-
-    if (grid[yy][xx] === '#') return true
-    if (grid[yy][xx] === 'L') break
-  }
-
-  for (let d = 1; ; d++) {
-    let xx = x - d
-    let yy = y + d
-    if (xx < 0) break
-    if (yy > HEIGHT - 1) break
-
-    if (grid[yy][xx] === '#') return true
-    if (grid[yy][xx] === 'L') break
-  }
-
-  for (let d = 1; ; d++) {
-    let xx = x - d
-    let yy = y - d
-    if (xx < 0) break
-    if (yy < 0) break
-
-    if (grid[yy][xx] === '#') return true
-    if (grid[yy][xx] === 'L') break
-  }
-
-  return false
-}
-
-function canBeFree(grid, x, y) {
-  let count = 0
-
-  for (let xx = x + 1; xx <= WIDTH - 1; xx++) {
-    if (grid[y][xx] === '#') {
-      count++
-      break
-    }
-    if (grid[y][xx] === 'L') break
-  }
-
-  for (let yy = y + 1; yy <= HEIGHT - 1; yy++) {
-    if (grid[yy][x] === '#') {
-      count++
-      break
-    }
-    if (grid[yy][x] === 'L') break
-  }
-
-  for (let xx = x - 1; xx >= 0; xx--) {
-    if (grid[y][xx] === '#') {
-      count++
-      break
-    }
-    if (grid[y][xx] === 'L') break
-  }
-
-  for (let yy = y - 1; yy >= 0; yy--) {
-    if (grid[yy][x] === '#') {
-      count++
-      break
-    }
-    if (grid[yy][x] === 'L') break
-  }
-
-  //
-
-  for (let d = 1; ; d++) {
-    let xx = x + d
-    let yy = y + d
-    if (xx > WIDTH - 1) break
-    if (yy > HEIGHT - 1) break
-
-    if (grid[yy][xx] === '#') {
-      count++
-      break
-    }
-    if (grid[yy][xx] === 'L') break
-  }
-
-  for (let d = 1; ; d++) {
-    let xx = x + d
-    let yy = y - d
-    if (xx > WIDTH - 1) break
-    if (yy < 0) break
-
-    if (grid[yy][xx] === '#') {
-      count++
-      break
-    }
-    if (grid[yy][xx] === 'L') break
-  }
-
-  for (let d = 1; ; d++) {
-    let xx = x - d
-    let yy = y + d
-    if (xx < 0) break
-    if (yy > HEIGHT - 1) break
-
-    if (grid[yy][xx] === '#') {
-      count++
-      break
-    }
-    if (grid[yy][xx] === 'L') break
-  }
-
-  for (let d = 1; ; d++) {
-    let xx = x - d
-    let yy = y - d
-    if (xx < 0) break
-    if (yy < 0) break
-
-    if (grid[yy][xx] === '#') {
-      count++
-      break
-    }
-    if (grid[yy][xx] === 'L') break
-  }
-
-  return count >= 5
-}
-
-function nextLevel() {
-  let changes = 0
-
-  // console.log('')
-  // console.log('')
-
-  for (let y = 0; y < HEIGHT; y++) {
-    for (let x = 0; x < WIDTH; x++) {
-      next[y][x] = current[y][x]
-
-      if (current[y][x] === 'L') {
-        if (!hasAdjacentOccupied(current, x, y)) {
-          changes++
-          next[y][x] = '#'
-        }
+  function hasAdjacentOccupied(grid, x, y) {
+    for (let yy = Math.max(0, y - 1); yy <= Math.min(HEIGHT - 1, y + 1); yy++) {
+      for (
+        let xx = Math.max(0, x - 1);
+        xx <= Math.min(WIDTH - 1, x + 1);
+        xx++
+      ) {
+        if (!(xx === x && yy === y) && grid[yy][xx] === '#') return true
       }
+    }
+    return false
+  }
 
-      if (current[y][x] === '#') {
-        if (canBeFree(current, x, y)) {
-          changes++
-          next[y][x] = 'L'
+  function canBeFree(grid, x, y) {
+    let count = 0
+
+    for (let yy = Math.max(0, y - 1); yy <= Math.min(HEIGHT - 1, y + 1); yy++) {
+      for (
+        let xx = Math.max(0, x - 1);
+        xx <= Math.min(WIDTH - 1, x + 1);
+        xx++
+      ) {
+        if (!(xx === x && yy === y) && grid[yy][xx] === '#') {
+          count++
+          if (count >= 4) return true
         }
       }
     }
+    return false
   }
 
-  let t = current
-  current = next
-  next = t
+  function nextLevel() {
+    let changes = 0
 
-  // current.forEach(line => {
-  //   console.log(line.join(''))
-  // })
+    for (let y = 0; y < HEIGHT; y++) {
+      for (let x = 0; x < WIDTH; x++) {
+        next[y][x] = current[y][x]
 
-  // console.log({changes})
-  return changes
-}
+        if (current[y][x] === 'L') {
+          if (!hasAdjacentOccupied(current, x, y)) {
+            changes++
+            next[y][x] = '#'
+          }
+        }
 
-// Wrong 2737
+        if (current[y][x] === '#') {
+          if (canBeFree(current, x, y)) {
+            changes++
+            next[y][x] = 'L'
+          }
+        }
+      }
+    }
 
-while (true) {
-  let changes = Math.max(nextLevel(), nextLevel(), nextLevel())
+    let t = current
+    current = next
+    next = t
 
-  if (changes === 0) {
-    // current.forEach(line => {
-    //   console.log(line.join(''))
-    // })
+    return changes
+  }
 
-    let sum = 0
-    current.forEach(line =>
-      line.forEach(c => {
-        if (c === '#') sum++
-      }),
-    )
-    console.log({sum})
-    process.exit(0)
+  while (true) {
+    let changes = Math.max(nextLevel(), nextLevel(), nextLevel())
+
+    if (changes === 0) {
+      let sum = 0
+      current.forEach(line =>
+        line.forEach(c => {
+          if (c === '#') sum++
+        }),
+      )
+      return sum
+    }
   }
 }
 
-// let fs = require('fs')
+function part2() {
+  let current = lines.map(f => f.slice())
+  let next = current.map(f => f.slice())
 
-// let current = fs
-//   .readFileSync('./11/input', {encoding: 'utf-8'})
-//   .split('\n')
-//   .filter(f => f.trim().length > 0)
-//   .map(f => f.split(''))
+  function hasAdjacentOccupied(grid, x, y) {
+    for (let xx = x + 1; xx <= WIDTH - 1; xx++) {
+      if (grid[y][xx] === '#') return true
+      if (grid[y][xx] === 'L') break
+    }
 
-// let next = current.map(f => f.slice())
+    for (let yy = y + 1; yy <= HEIGHT - 1; yy++) {
+      if (grid[yy][x] === '#') return true
+      if (grid[yy][x] === 'L') break
+    }
 
-// const WIDTH = current[0].length
-// const HEIGHT = current.length
+    for (let xx = x - 1; xx >= 0; xx--) {
+      if (grid[y][xx] === '#') return true
+      if (grid[y][xx] === 'L') break
+    }
 
-// function hasAdjacentOccupied(grid, x, y) {
-//   for (let yy = Math.max(0, y - 1); yy <= Math.min(HEIGHT - 1, y + 1); yy++) {
-//     for (let xx = Math.max(0, x - 1); xx <= Math.min(WIDTH - 1, x + 1); xx++) {
-//       if (!(xx === x && yy === y) && grid[yy][xx] === '#') return true
-//     }
-//   }
-//   return false
-// }
+    for (let yy = y - 1; yy >= 0; yy--) {
+      if (grid[yy][x] === '#') return true
+      if (grid[yy][x] === 'L') break
+    }
 
-// function canBeFree(grid, x, y) {
-//   let count = 0
+    //
 
-//   for (let yy = Math.max(0, y - 1); yy <= Math.min(HEIGHT - 1, y + 1); yy++) {
-//     for (let xx = Math.max(0, x - 1); xx <= Math.min(WIDTH - 1, x + 1); xx++) {
-//       if (!(xx === x && yy === y) && grid[yy][xx] === '#') {
-//         count++
-//         if (count >= 4) return true
-//       }
-//     }
-//   }
-//   return false
-// }
+    for (let d = 1; ; d++) {
+      let xx = x + d
+      let yy = y + d
+      if (xx > WIDTH - 1) break
+      if (yy > HEIGHT - 1) break
 
-// function nextLevel() {
-//   let changes = 0
+      if (grid[yy][xx] === '#') return true
+      if (grid[yy][xx] === 'L') break
+    }
 
-//   for (let y = 0; y < HEIGHT; y++) {
-//     for (let x = 0; x < WIDTH; x++) {
-//       next[y][x] = current[y][x]
+    for (let d = 1; ; d++) {
+      let xx = x + d
+      let yy = y - d
+      if (xx > WIDTH - 1) break
+      if (yy < 0) break
 
-//       if (current[y][x] === 'L') {
-//         if (!hasAdjacentOccupied(current, x, y)) {
-//           changes++
-//           next[y][x] = '#'
-//         }
-//       }
+      if (grid[yy][xx] === '#') return true
+      if (grid[yy][xx] === 'L') break
+    }
 
-//       if (current[y][x] === '#') {
-//         if (canBeFree(current, x, y)) {
-//           changes++
-//           next[y][x] = 'L'
-//         }
-//       }
-//     }
-//   }
+    for (let d = 1; ; d++) {
+      let xx = x - d
+      let yy = y + d
+      if (xx < 0) break
+      if (yy > HEIGHT - 1) break
 
-//   let t = current
-//   current = next
-//   next = t
+      if (grid[yy][xx] === '#') return true
+      if (grid[yy][xx] === 'L') break
+    }
 
-//   // console.log('')
-//   // console.log('')
-//   // current.forEach(line => {
-//   //   console.log(line.join(''))
-//   // })
+    for (let d = 1; ; d++) {
+      let xx = x - d
+      let yy = y - d
+      if (xx < 0) break
+      if (yy < 0) break
 
-//   // console.log({changes})
-//   return changes
-// }
+      if (grid[yy][xx] === '#') return true
+      if (grid[yy][xx] === 'L') break
+    }
 
-// // Wrong 2367
-// // 2367
+    return false
+  }
 
-// while (true) {
-//   let changes = Math.max(nextLevel(), nextLevel(), nextLevel())
+  function canBeFree(grid, x, y) {
+    let count = 0
 
-//   if (changes === 0) {
-//     // current.forEach(line => {
-//     //   console.log(line.join(''))
-//     // })
+    for (let xx = x + 1; xx <= WIDTH - 1; xx++) {
+      if (grid[y][xx] === '#') {
+        count++
+        break
+      }
+      if (grid[y][xx] === 'L') break
+    }
 
-//     let sum = 0
-//     current.forEach(line =>
-//       line.forEach(c => {
-//         if (c === '#') sum++
-//       }),
-//     )
-//     console.log({sum})
-//     process.exit(0)
-//   }
-// }
+    for (let yy = y + 1; yy <= HEIGHT - 1; yy++) {
+      if (grid[yy][x] === '#') {
+        count++
+        break
+      }
+      if (grid[yy][x] === 'L') break
+    }
+
+    for (let xx = x - 1; xx >= 0; xx--) {
+      if (grid[y][xx] === '#') {
+        count++
+        break
+      }
+      if (grid[y][xx] === 'L') break
+    }
+
+    for (let yy = y - 1; yy >= 0; yy--) {
+      if (grid[yy][x] === '#') {
+        count++
+        break
+      }
+      if (grid[yy][x] === 'L') break
+    }
+
+    //
+
+    for (let d = 1; ; d++) {
+      let xx = x + d
+      let yy = y + d
+      if (xx > WIDTH - 1) break
+      if (yy > HEIGHT - 1) break
+
+      if (grid[yy][xx] === '#') {
+        count++
+        break
+      }
+      if (grid[yy][xx] === 'L') break
+    }
+
+    for (let d = 1; ; d++) {
+      let xx = x + d
+      let yy = y - d
+      if (xx > WIDTH - 1) break
+      if (yy < 0) break
+
+      if (grid[yy][xx] === '#') {
+        count++
+        break
+      }
+      if (grid[yy][xx] === 'L') break
+    }
+
+    for (let d = 1; ; d++) {
+      let xx = x - d
+      let yy = y + d
+      if (xx < 0) break
+      if (yy > HEIGHT - 1) break
+
+      if (grid[yy][xx] === '#') {
+        count++
+        break
+      }
+      if (grid[yy][xx] === 'L') break
+    }
+
+    for (let d = 1; ; d++) {
+      let xx = x - d
+      let yy = y - d
+      if (xx < 0) break
+      if (yy < 0) break
+
+      if (grid[yy][xx] === '#') {
+        count++
+        break
+      }
+      if (grid[yy][xx] === 'L') break
+    }
+
+    return count >= 5
+  }
+
+  function nextLevel() {
+    let changes = 0
+
+    for (let y = 0; y < HEIGHT; y++) {
+      for (let x = 0; x < WIDTH; x++) {
+        next[y][x] = current[y][x]
+
+        if (current[y][x] === 'L') {
+          if (!hasAdjacentOccupied(current, x, y)) {
+            changes++
+            next[y][x] = '#'
+          }
+        }
+
+        if (current[y][x] === '#') {
+          if (canBeFree(current, x, y)) {
+            changes++
+            next[y][x] = 'L'
+          }
+        }
+      }
+    }
+
+    let t = current
+    current = next
+    next = t
+
+    return changes
+  }
+
+  while (true) {
+    let changes = Math.max(nextLevel(), nextLevel(), nextLevel())
+
+    if (changes === 0) {
+      let sum = 0
+      current.forEach(line =>
+        line.forEach(c => {
+          if (c === '#') sum++
+        }),
+      )
+      return sum
+    }
+  }
+}
+
+console.log('Part 1', part1())
+console.log('Part 2', part2())

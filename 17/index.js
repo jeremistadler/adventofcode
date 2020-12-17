a7b027944b529fe22661ecdef4c4780d54412932
Jeremi Stadler <jeremi.stadler@storytel.com>
2020-12-17 07:55:58 +0100

    2020 Day 17

---
+++ 17/index.js
@@ -0,0 +1,200 @@
+let input = require('fs')
+  .readFileSync('./17/input', 'utf-8')
+  .split('\n')
+  .map(f => f.trim())
+  .filter(f => f.length > 0)
+  .map(f => f.split(''))
+
+function part1() {
+  let current = new Map()
+  let next = new Map()
+
+  input.forEach((line, lineIndex) => {
+    line.forEach((letter, letterIndex) => {
+      if (letter === '#')
+        current.set(xyzToMap(letterIndex, lineIndex, 0), [
+          letterIndex,
+          lineIndex,
+          0,
+        ])
+    })
+  })
+
+  let checked = new Set()
+
+  function checkItem(x, y, z) {
+    let count = countNeightbours(current, x, y, z)
+    const index = xyzToMap(x, y, z)
+    let c = current.has(index)
+
+    // if (z === 0 && y === 1 && x === 2) console.log({x, y, z, count, index, c})
+
+    if (c === true) {
+      if (count === 2 || count === 3) {
+        next.set(index, [x, y, z])
+      }
+    } else {
+      if (count === 3) {
+        next.set(index, [x, y, z])
+      }
+    }
+
+    if (count === 0) return
+
+    for (let xx = x - 1; xx <= x + 1; xx++) {
+      for (let yy = y - 1; yy <= y + 1; yy++) {
+        for (let zz = z - 1; zz <= z + 1; zz++) {
+          const i = xyzToMap(xx, yy, zz)
+          if (checked.has(i) === false) {
+            checked.add(i)
+            checkItem(xx, yy, zz)
+          }
+        }
+      }
+    }
+  }
+
+  for (let i = 0; ; i++) {
+    next.clear()
+    checked.clear()
+
+    for (const [, item] of current) {
+      let [x, y, z] = item
+      checkItem(x, y, z)
+    }
+
+    let tmp = next
+    next = current
+    current = tmp
+
+    if (i === 5) return current.size
+  }
+
+  function countNeightbours(grid, x, y, z) {
+    let count = 0
+
+    for (let xx = x - 1; xx <= x + 1; xx++) {
+      for (let yy = y - 1; yy <= y + 1; yy++) {
+        for (let zz = z - 1; zz <= z + 1; zz++) {
+          if (
+            !(xx === x && yy === y && zz === z) &&
+            grid.has(xyzToMap(xx, yy, zz))
+          ) {
+            count++
+          }
+        }
+      }
+    }
+
+    return count
+  }
+
+  function xyzToMap(x, y, z) {
+    return x * 1000000 + y * 1000 + z
+  }
+}
+
+function part2() {
+  let current = new Map()
+  let next = new Map()
+
+  input.forEach((line, lineIndex) => {
+    line.forEach((letter, letterIndex) => {
+      if (letter === '#')
+        current.set(xyzwToMap(letterIndex, lineIndex, 0, 0), [
+          letterIndex,
+          lineIndex,
+          0,
+          0,
+        ])
+    })
+  })
+
+  let checked = new Set()
+  let queue = []
+
+  function checkItem(x, y, z, w) {
+    let count = countNeightbours(current, x, y, z, w)
+    const index = xyzwToMap(x, y, z, w)
+    let c = current.has(index)
+
+    if (c === true) {
+      if (count === 2 || count === 3) {
+        next.set(index, [x, y, z, w])
+      }
+    } else {
+      if (count === 3) {
+        next.set(index, [x, y, z, w])
+      }
+    }
+
+    if (count === 0) return
+
+    for (let xx = x - 1; xx <= x + 1; xx++) {
+      for (let yy = y - 1; yy <= y + 1; yy++) {
+        for (let zz = z - 1; zz <= z + 1; zz++) {
+          for (let ww = w - 1; ww <= w + 1; ww++) {
+            const i = xyzwToMap(xx, yy, zz, ww)
+            if (checked.has(i) === false) {
+              checked.add(i)
+              queue.push([xx, yy, zz, ww])
+            }
+          }
+        }
+      }
+    }
+  }
+
+  for (let i = 0; ; i++) {
+    next.clear()
+    checked.clear()
+
+    for (const [, item] of current) {
+      let [x, y, z, w] = item
+
+      const i = xyzwToMap(x, y, z, w)
+      checked.add(i)
+      queue.push(item)
+    }
+
+    while (queue.length > 0) {
+      let [x, y, z, w] = queue.pop()
+      checkItem(x, y, z, w)
+    }
+
+    let tmp = next
+    next = current
+    current = tmp
+
+    if (i === 5) return current.size
+  }
+
+  function countNeightbours(grid, x, y, z, w) {
+    let count = 0
+
+    for (let xx = x - 1; xx <= x + 1; xx++) {
+      for (let yy = y - 1; yy <= y + 1; yy++) {
+        for (let zz = z - 1; zz <= z + 1; zz++) {
+          for (let ww = w - 1; ww <= w + 1; ww++) {
+            if (
+              !(xx === x && yy === y && zz === z && ww === w) &&
+              grid.has(xyzwToMap(xx, yy, zz, ww))
+            ) {
+              count++
+            }
+          }
+        }
+      }
+    }
+
+    return count
+  }
+
+  function xyzwToMap(x, y, z, w) {
+    if (typeof w === 'undefined' || isNaN(w)) throw new Error('Is Nan!')
+    return w * 1000000000 + x * 1000000 + y * 1000 + z
+  }
+}
+
+console.log('Part 1', part1())
+console.log('Part 2', part2())
